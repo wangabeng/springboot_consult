bf247fa6875f6d4f66a30de625ea836f073a06ce
wangabeng <wangabeng@163.com>
2019-09-12 00:12:02 +0800

    update

--- src/main/java/com/runjie/consult/exception/AdminException.java
+++ src/main/java/com/runjie/consult/exception/AdminException.java
@@ -1,5 +1,25 @@
 package com.runjie.consult.exception;
 
-public class AdminException {
+import com.runjie.consult.enums.ResultEnum;
 
+public class AdminException extends RuntimeException {
+	private Integer code;
+
+	public AdminException(ResultEnum resultEnum) {
+		super(resultEnum.getMessage());
+		this.code = resultEnum.getCode();
+	}
+
+	public AdminException(Integer code, String message) {
+		super(message);
+		this.code = code;
+	}
+
+	public Integer getCode() {
+		return code;
+	}
+
+	public void setCode(Integer code) {
+		this.code = code;
+	}
 }
